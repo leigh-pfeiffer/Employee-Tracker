82f44b3748c55500722bfb9c1be617e9aa1b2656
Leigh Pfeiffer <leigh.pfeiffer@gmail.com>
2022-03-26 22:19:24 -0400

    seeds file and linking SQL... i think

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO department (name)
+VALUES ('Sales'), ('Engineering'), ('Legal'), ('Finance');
+
+INSERT role (title, salary)
+VALUES ('Sales Lead', 100000), ('Salesperson', 80000), ('Lead Engineer', 150000), ('Software Engineer', 120000), ('Accountant', 125000), ('Legal Team Lead', 250000), ('Lawyer', 190000);
+
+INSERT employee (first_name, last_name)
+VALUES ('John', 'Doe'), ('Mike', 'Chan'), ('Ashley', 'Rodriguez'), ('Kevin', 'Tupik'), ('Malia', 'Brown'), ('Sarah', 'Lourd'), ('Tom', 'Allen'), ('Tammer', 'Galal'), ('Christian', 'Eckenrode');
+
